594fc7d525f1f4415b439dc3d0c2f5b64b253772
upadhyan <49886694+upadhyan@users.noreply.github.com>
2022-03-14 21:24:13 -0400

    folder structure

---
+++ Heterogenous Clusters/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Creating an algorithm to group data points based on how different they are',
+    author='Nakul Upadhya and Devon Herr',
+    license='',
+)
